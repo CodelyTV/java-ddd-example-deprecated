cdbd8baa5f7881c17f6699c76f70d4f91dc70377
Rafa Gómez <rgomezcasas@gmail.com>
2019-03-09 18:21:03 +0100

    Add student id to review

--- src/mooc/main/tv/codely/mooc/course/domain/Review.java
+++ src/mooc/main/tv/codely/mooc/course/domain/Review.java
@@ -1,14 +1,18 @@
 package tv.codely.mooc.course.domain;
 
-final public class Review {
-    private final ReviewId id;
-    private final Rating   rating;
-    private final Comment  comment;
+import tv.codely.shared.domain.students.StudentId;
 
-    public Review(ReviewId id, Rating rating, Comment comment) {
-        this.id      = id;
-        this.rating  = rating;
-        this.comment = comment;
+final public class Review {
+    private final ReviewId  id;
+    private final Rating    rating;
+    private final Comment   comment;
+    private final StudentId studentId;
+
+    public Review(ReviewId id, Rating rating, Comment comment, StudentId studentId) {
+        this.id        = id;
+        this.rating    = rating;
+        this.comment   = comment;
+        this.studentId = studentId;
     }
 
     public ReviewId id() {
@@ -21,5 +25,9 @@ final public class Review {
 
     public Comment comment() {
         return comment;
+    }
+
+    public StudentId studentId() {
+        return studentId;
     }
 }
---
+++ src/shared/main/tv.codely.shared/domain/students/StudentId.java
@@ -0,0 +1,9 @@
+package tv.codely.shared.domain.students;
+
+import tv.codely.shared.domain.Identifier;
+
+final public class StudentId extends Identifier {
+    public StudentId(String value) {
+        super(value);
+    }
+}
