49d924ae9c9fa229f67be0461f67a5769cc2796b
Xisco Bibiloni <neo.xisco@gmail.com>
2022-05-08 10:46:52 +0200

    feat:Add new Port to publish video in "CustomMeta"

--- src/mooc/test/tv/codely/mooc/video/application/publish/VideoPublisherShould.java
+++ src/mooc/test/tv/codely/mooc/video/application/publish/VideoPublisherShould.java
@@ -2,6 +2,7 @@ package tv.codely.mooc.video.application.publish;
 
 import org.junit.jupiter.api.Test;
 import tv.codely.mooc.video.domain.VideoPublished;
+import tv.codely.mooc.video.domain.port.VideoManagementPort;
 import tv.codely.shared.domain.EventBus;
 
 import java.util.List;
@@ -13,7 +14,8 @@ final class VideoPublisherShould {
     @Test
     void publish_the_video_published_domain_event() {
         final EventBus eventBus = mock(EventBus.class);
-        final var videoPublisher = new VideoPublisher(eventBus);
+        final VideoManagementPort videoManagementPort = mock(VideoManagementPort.class);
+        final var videoPublisher = new VideoPublisher(eventBus, videoManagementPort);
 
         final var videoTitle = "\uD83C\uDF89 New YouTube.com/CodelyTV video title";
         final var videoDescription = "This should be the video description \uD83D\uDE42";
