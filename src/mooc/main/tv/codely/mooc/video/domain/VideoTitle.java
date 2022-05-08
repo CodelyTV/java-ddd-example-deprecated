25eab8ebc2f137e13b3792530df187df4941e8ac
Xisco Bibiloni <neo.xisco@gmail.com>
2022-05-08 10:41:33 +0200

    refactor:Configure lombok

--- src/mooc/main/tv/codely/mooc/video/domain/VideoTitle.java
+++ src/mooc/main/tv/codely/mooc/video/domain/VideoTitle.java
@@ -1,33 +1,13 @@
 package tv.codely.mooc.video.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
+@AllArgsConstructor
 public final class VideoTitle {
     private final String value;
 
-    public VideoTitle(String value) {
-        this.value = value;
-    }
-
-    public String value() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        VideoTitle that = (VideoTitle) o;
-
-        return value.equals(that.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return value.hashCode();
-    }
 }
